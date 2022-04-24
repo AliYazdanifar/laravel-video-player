313ff9b3d9272ec2fe9e344d9cc715d8ab79d288
ali yazdanifar <ali.yazdani324@gmail.com>
2022-04-24 10:30:36 +0430

    end of emkanat avaliye

--- database/migrations/2022_01_21_094348_create_categories_table.php
+++ database/migrations/2022_01_21_094348_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->string('slug',128)->nullable();
+            $table->string('slug',128)->unique();
             $table->string('icon')->nullable();
 
             $table->timestamps();
